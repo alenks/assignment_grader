0d91ce7f1b019d5bd95a7887560fc566ad2a47e0
Alen KS <alens@comp.nus.edu.sg>
2020-09-12 14:17:10 +0800

    [evaluation.py] Removed unnecessary print statements

--- evaluation.py
+++ evaluation.py
@@ -1,20 +1,14 @@
 def ex1(infile_c, workdir, sol_dir):
-  print(infile_c)
-  print('ex1')
-  return 10
+  return 1
 
 def ex2(infile_c, workdir, sol_dir):
-  print('ex2')
-  return 12
+  return 2
 
 def ex3(infile_c, workdir, sol_dir):
-  print('ex3')
-  return 16
+  return 3
 
 def ex4(infile_c, workdir, sol_dir):
-  print('ex4')
-  return 20
+  return 4
 
 def ex5(infile_c, workdir, sol_dir):
-  print('ex5')
-  return 24
+  return 5
